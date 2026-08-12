360418b1f340b1a2687244ee546bf4c1c711ab77
lotsofnoise <87506774+lotsofnoise@users.noreply.github.com>
2026-08-12 06:53:30 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
 // requires the OS permission grant, but no network is needed for that.
 // ---------------------------------------------------------------------------
 
-const CACHE_NAME = 'horizon-camera-v6j';
+const CACHE_NAME = 'horizon-camera-v6k';
 const APP_SHELL = [
   './',
   './index.html',
